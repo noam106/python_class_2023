7bff1c1e359245d19bb37122e47349ea4a946a8d
noam106 <noamps30@gmail.com>
2023-01-09 16:30:24 +0200

    home computer

--- leson_050123/homework.py
+++ leson_050123/homework.py
@@ -28,5 +28,43 @@ class Space(ABC):
 
 class Room(Space):
 
+    def __init__(self, size: float, width: float, length: float, windows_amount: int,
+                 has_balcony_door: bool):
+        super().__init__(size, width, length)
+        self._window_amount = windows_amount
+        self._has_balcony_door = has_balcony_door
 
-    def __init__(self, ):
+    def get_has_balcony_door(self):
+        return self._has_balcony_door
+
+    def get_window_amount(self):
+        return self._window_amount
+
+
+class Bath(Room):
+
+    def __init__(self, size: float, width: float, length, float, window_amount: int,
+                 has_balcony_door: bool):
+        super().__init__(size, width, length, window_amount, has_balcony_door)
+
+        self._facilities = {
+            toilet : 0,
+            sink : 0,
+            bath : 0,
+            shower : 0
+        }
+
+    def get_facilities(self):
+        return self._facilities
+
+    def set_facilities(self, facil, num_of_facil):
+        if facil in self._facilities:
+            self._facilities[facil] = num_of_facil
+
+
+class Kitchen(Room):
+
+    def __init__(self, size: float, width: float, length, float, window_amount: int,
+                 has_balcony_door: bool):
+        super().__init__(size, width, length, window_amount, has_balcony_door)
+
