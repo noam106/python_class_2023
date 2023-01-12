0ef79ea7bf02aa5cbe3e32947965e9baa06b5e11
noam106 <117109080+noam106@users.noreply.github.com>
2023-01-12 19:51:20 +0200

    from home

--- leson_050123/homework.py
+++ leson_050123/homework.py
@@ -65,6 +65,24 @@ class Bath(Room):
 class Kitchen(Room):
 
     def __init__(self, size: float, width: float, length: float, window_amount: int,
-                 has_balcony_door: bool):
+                 has_balcony_door: bool, amount_upper_closets: float, amount_lower_closets: float,
+                 material_work_surface: str):
         super().__init__(size, width, length, window_amount, has_balcony_door)
+        self._material_work_surface = material_work_surface
+        self._amount_lower_closets = amount_lower_closets
+        self._amount_upper_closets = amount_upper_closets
+        self._facilities = {
+            'toilet': 0,
+            'sink': 0,
+            'bath': 0,
+            'shower': 0
+        }
+
+        def get_facilities(self):
+            return self._facilities
+
+        def set_facilities(self, facil, num_of_facil):
+            if facil in self._facilities:
+                self._facilities[facil] = num_of_facil
+
 
