6f13415f1bf979ef07fc1b2926b6cebb7e94d3aa
noam106 <117109080+noam106@users.noreply.github.com>
2023-01-14 16:38:48 +0200

    from home

--- leson_050123/homework.py
+++ leson_050123/homework.py
@@ -1,6 +1,25 @@
 from abc import ABC, abstractmethod
 
 
+class Address:
+
+def __init__(self, street, city, floor, zipcode):
+    self._street = street
+    self._city = city
+    self._floor = floor
+    self._zipcode = zipcode
+
+def get_street(self):
+    return self._street
+
+def get_city(self):
+    return self._city
+
+def get_floor(self):
+    return self._floor
+
+def get_zipcode(self):
+    return self._zipcode
 class Space(ABC):
 
     def __init__(self, size: float, width: float, length: float):
@@ -25,6 +44,7 @@ class Space(ABC):
 
     def set_length(self, new_length):
         self._length = new_length
+
 
 class Room(Space):
 
@@ -71,18 +91,84 @@ class Kitchen(Room):
         self._material_work_surface = material_work_surface
         self._amount_lower_closets = amount_lower_closets
         self._amount_upper_closets = amount_upper_closets
-        self._facilities = {
-            'toilet': 0,
-            'sink': 0,
-            'bath': 0,
-            'shower': 0
-        }
+        self._kitchen_island = False
+        self._k_facilities = {
+            'built-in oven': 0,
+            'stove': 0,
+            'dishwasher': 0,
+            }
 
-        def get_facilities(self):
-            return self._facilities
+    def get_material_work_surface(self):
+        return self._material_work_surface
 
-        def set_facilities(self, facil, num_of_facil):
-            if facil in self._facilities:
-                self._facilities[facil] = num_of_facil
+    def set_material_work_surface(self, new_material: str):
+        self._material_work_surface = new_material
 
+    def get_amount_lower_closets(self):
+        return self._amount_lower_closets
+
+    def set_amount_lower_closets(self, new_size: float):
+        self._amount_lower_closets = new_size
+
+    def get_amount_upper_closets(self):
+        return self._amount_upper_closets
+
+    def set_amount_upper_closets(self, new_size: float):
+        self._amount_upper_closets = new_size
+
+    def get_k_facilities(self):
+        return self._k_facilities
+
+    def set_k_facilities(self, facil, num_of_facil):
+        if facil in self._k_facilities:
+            self._k_facilities[facil] = num_of_facil
+
+    def get_kitchen_island(self):
+        return self._kitchen_island
+
+    def set_kitchen_island(self, is_kitchen_island: bool):
+        self._kitchen_island = is_kitchen_island
+
+
+class Balcony(Space):
+
+    def __init__(self,size: float, width: float, length: float, has_gas: bool, has_water: bool):
+        super().__init__(size, width, length)
+        self._has_gas = has_gas
+        self._has_water = has_water
+
+    def get_has_gas(self):
+        return self._has_gas
+
+    def set_has_gas(self, is_gas: bool):
+        self._has_gas = is_gas
+
+    def get_has_water(self):
+        return self._has_water
+
+    def set_has_water(self, is_water: bool):
+        self._has_water = is_water
+
+
+def get_floor_num():
+    return get_floor()
+
+
+class Flat():
+
+    def __init__(self, address: Address, num_of_floor: int):
+        self._address = address
+        self._num_of_floor = num_of_floor
+        self._dict_of_room: dict[int, Room] = {}
+
+    def get_address(self):
+        return self._address
+
+    def get_num_of_floors(self):
+        return self._num_of_floor
+
+    def get_dict_of_room(self):
+        return self._dict_of_room
+
+    def add_room(self,):
 
