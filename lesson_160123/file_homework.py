5f119706891a53ce92b8fcc2da746add74480290
noam106 <117109080+noam106@users.noreply.github.com>
2023-01-19 17:05:24 +0200

    from home

--- lesson_160123/file_homework.py
+++ lesson_160123/file_homework.py
@@ -1,8 +1,9 @@
 import os
+import datetime
 
 
 def scv_search(path_to_file: str) -> dict:
-    val_dict: dict[str,dict] = {}
+    val_dict: dict[str, dict] = {}
     if os.path.exists(path_to_file):
         for root, dirs, files in os.walk(path_to_file):
             for name in files:
@@ -18,3 +19,47 @@ def scv_search(path_to_file: str) -> dict:
         return val_dict
     else:
         return None
+
+
+print(scv_search('C:\\Users\\USER001\\Downloads\\D1_TEST'))
+
+
+def apple_stock_sum(stock_file_path: str) -> str:
+
+    temp_max = 0
+    temp_min = 0
+    temp_volumes = 0
+    count = 0
+    average_dict: dict[datetime, dict] = {}
+    year_1 = None
+    with open(stock_file_path, 'r') as apple_stock:
+        apple_stock.readline()
+        for line in apple_stock:
+
+            line_date = datetime.datetime.strptime(line[0], '%d-%m-%Y')
+            year = line_date.year
+            if year_1 is None:
+                year_1 = year
+                if year_1 == year:
+                    temp_min += line[1]
+                    temp_volumes += line[4]
+                    temp_max += line[3]
+                    count += 1
+                else:
+                    average_dict[year_1] = {
+                        'Minimum price': temp_min/count,
+                        'Max price': temp_max / count,
+                        'volumes': temp_volumes / count
+                    }
+                    year_1 = year
+                    count = 0
+                    temp_volumes = 0
+                    temp_min = 0
+                    temp_max = 0
+    return average_dict
+
+
+print(apple_stock_sum("C:\\Users\\USER001\\Downloads\\AAPL.csv"))
+
+
+
